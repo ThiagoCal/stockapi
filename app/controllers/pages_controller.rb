e07a3f44b8d7b816e34650d3dfaba03733af1cac
Thiago Calazans <thiagotorrescalazans@gmail.com>
2022-05-11 00:58:01 -0300

    creating stock market app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,20 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :index ]
 
   def home
   end
+
+  def index
+    @api = StockQuote::Stock.new(api_key: "pk_03f609bd3ac74f70b35c77418535c946")
+    if params[:ticker] == ""
+      @nothing = "You forgot to enter a Symbol"
+    elsif params[:ticker]
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+      rescue => e
+        @error = "Sorry, maybe you should try again, the symbol you wrote doesn't exist"
+      end
+   end
+  end
+
 end
